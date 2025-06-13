f166dfee9024564a8e1577de60605897b2f34dbe
KhanRabiul <krabiulislam2@gmail.com>
2025-06-13 22:26:29 +0600

    backend() [] backend connected successfully

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,24 @@
-import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
-
 dotenv.config();
 
-const  app = express();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import authRoues from './routes/authRoutes';
+import * as jwtUtils from './utils/jwt';
+
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI as string)
-.then(() => console.log('MongoDB connected successfully'))
-.catch((err) => console.log(err));
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoues);
 
 app.get('/', (req, res) => {
   res.send('RenNest backend API is running');
