87ebd4088ce5d332ef3d1d53e692ff895b4aa2e7
KhanRabiul <krabiulislam2@gmail.com>
2025-06-28 22:01:44 +0600

    frontend() [signup: page.tsx] sign up page style and logic

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,34 @@
+'use client';
+
+import { useAuth } from "@/contexts/AuthContext";
 import SignUpForm from "@/ui/auth/SignUpForm";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const SignUpPage = () => {
+
+  const { isLoggedIn, isLoading: authLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (authLoading) return;
+
+    if (isLoggedIn) {
+      router.push('/profile');
+    }
+  },
+    [isLoggedIn, authLoading, router]);
+
+  if (authLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-[--background] text-[--foreground]">
+        <p>Loading registration page...</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center bg-[--background] text-[--foreground]">
       <SignUpForm />
     </div>
   );
